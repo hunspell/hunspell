75bb3bd0f868d867fb66d4d4eacfb9f1f2b2fdb7
Caolán McNamara <caolanm@redhat.com>
2010-08-10 08:04:51 +0000

    hun#3022860 fix remove duplicate code

--- src/win_api/config.h
+++ src/win_api/config.h
@@ -204,5 +204,5 @@
 #define PACKAGE_TARNAME
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.2.12"
-#define VERSION "1.2.12"
+#define PACKAGE_VERSION "1.2.13"
+#define VERSION "1.2.13"
