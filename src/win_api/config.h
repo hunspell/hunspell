4181174b1293ffceda99775e513fd92b53f4bba5
László Németh <nemeth@numbertext.org>
2011-01-26 13:59:31 +0000

    commit for release 1.3 (code of 1.2.15 and 1.3 are identical)

--- src/win_api/config.h
+++ src/win_api/config.h
@@ -204,5 +204,5 @@
 #define PACKAGE_TARNAME
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.2.15"
-#define VERSION "1.2.15"
+#define PACKAGE_VERSION "1.3.0"
+#define VERSION "1.3.0"
