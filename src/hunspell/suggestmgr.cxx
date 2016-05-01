cb1535b90239e7d5d47a69cbf096c37b4be5522d
Dimitrij Mijoski <dmjpp@hotmail.com>
2016-05-01 14:56:30 +0200

    Past begin() iterator decrement error
    VS Debug build threw error on decrement past begin.

--- src/hunspell/suggestmgr.cxx
+++ src/hunspell/suggestmgr.cxx
@@ -964,10 +964,10 @@ int SuggestMgr::movechar(std::vector<std::string>& wlst,
     std::copy(word, word + candidate.size(), candidate.begin());
   }
 
-  for (std::string::iterator p = candidate.begin() + candidate.size() - 1; p > candidate.begin(); --p) {
-    for (std::string::iterator q = p - 1; q >= candidate.begin() && std::distance(q, p) < 10; --q) {
-      std::swap(*q, *(q + 1));
-      if (std::distance(q, p) < 2)
+  for (std::string::reverse_iterator p = candidate.rbegin(), pEnd = candidate.rend() - 1; p != pEnd; ++p) {
+    for (std::string::reverse_iterator q = p + 1, qEnd = candidate.rend(); q != qEnd && std::distance(p, q) < 10; ++q) {
+      std::swap(*q, *(q - 1));
+      if (std::distance(p, q) < 2)
         continue;  // omit swap char
       testsug(wlst, candidate, cpdsuggest, NULL, NULL);
     }
