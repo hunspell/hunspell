2213ac88cf27bcbbabe70c33e6a35e6bf3c381af
Dimitrij Mijoski <dmjpp@hotmail.com>
2017-01-31 13:33:14 +0100

    v2: Better definition of interface for spell()

--- src/hunspell2/hunspell.hxx
+++ src/hunspell2/hunspell.hxx
@@ -1,5 +1,4 @@
 #include <string>
-#include <locale>
 
 namespace hunspell {
 
@@ -9,9 +8,24 @@ enum spell_result {
 
 class hunspell {
 
+public:
 	using string = std::string;
 	using u16string = std::u16string;
-	using locale = std::locale;
+
+private:
+
+	/* (0)
+	All the major work is done here.
+	(1) and (2) are the lowest level specializations.
+	The rest just do some conversions and delegate to them.
+
+	(1) will simply call this
+	with ConversionIterator set to string::iterator
+
+	(2) will call this with u8_u32 on the fly conversion iterator.
+	*/
+	template <class ConvIter>
+	spell(ConvIter start, ConvIter end, const string& s);
 
 	/**
 	 (1) This should be called when the input and the dictionary
@@ -41,9 +55,8 @@ class hunspell {
 	 (4) This should be called when the input is
 	 single-byte narow OR multi-byte narrow string.
 	 and the dictionary is UTF-8
-	 Locale object should help identify the encoding of the input string.
-	 Generally, a loseless conversion should happend from narrow string
-	 to UTF-8, and (2) should be called.
+	 The input can be anything so we must use some info about the input
+	 encoding, a C locale od C++ locale object.
 
 	 One can do narrow -> u16 -> u8 like this:
 	 get old C locale,
@@ -52,11 +65,15 @@ class hunspell {
 	 revert old C locale,
 	 then codecvt<char16_t, char, mbstate_t>
 
-	 We might completely exclude this variant.
-	*/
-	spell_result
-	spell_narrow_input_u8_dict(const string& word, const locale& loc);
+	 There is no C++ way to go mbr to u16, we're limited to mbrtoc16.
 
+	 For that reason we will do similar conversion,
+	 but in a more high level public funcrion.
+	 Tis function should be UNUSED.
+	*/
+	//spell_result spell_narrow_input_u8_dict(const string& word);
+
+public:
 	/**
 	 (5) This should be called when the input and the dictionary
 	 are in the same encoding which can be single byte or UTF-8.
@@ -67,50 +84,37 @@ class hunspell {
 	spell(const string& word);
 
 	/**
-	 (6) Unknown narrow input
-	 If dict is utf-8 call (4)
-	 Else do narrow -> u16 -> u8 and call (3).
-
-	 We might completely exclude this variant.
+	 (6) Unknown narrow input (single byte or multi byte).
+	 Use current C locale and mbrtoc16 to convert it to known.
+	 Do a conversion mbr -> u16 -> u8.
+	 Use mbrtoc16, codecvt<char16_t, char, mbstate_t>
+	 We can check if the the current locale is already utf-8 to skip this.
+	 
+	 Once we know we have a u8 string, just call (7).
+	 
+	 This should be the recomended way to interface with the command line
+	 utility. Before calling this function, one should call
+	 setlocale(LC_ALL, "") or locale::global(locale("")).
+	 If we use std::cin, we should imbue it with cin.imbue(locale())
 	*/
 	spell_result
-	spell_narrow_input(const string& word, const locale& loc);
+	spell_narrow_input(const string& word);
 
 	/**
 	 (7) UTF-8 input. Will delegate either to (2) or (3).
 	*/
 	spell_result
 	spell_u8_input(const string& word);
-
-
-	/* All the major work is done in (1) and (2). The rest just do
-	some conversions and delegate to them.
-	And (1) and (2) can be implemented with a single generic method.
-
-	template <class ConversionIterator>
-	spell(ConversionIterator start, ConversionIterator end, string s)
-
-	(1) will simply call this
-	with ConversionIterator set to string::iterator
-
-	(2) will call this with u8_u32 on the fly conversion iterator.
-	*/
-
-	/**
-	 (8)
-	*/
+private:
+	/** (8) */
 	spell_result
 	spell_u16_input_singlechar_dict(const u16string& word);
 
-	/**
-	 (9)
-	*/
+	/** (9) */
 	spell_result
 	spell_u16_input_u8_dict(const u16string& word);
-
-	/**
-	 (10)
-	*/
+public:
+	/** (10) */
 	spell_result
 	spell_u16_input(const u16string& word);
 };
