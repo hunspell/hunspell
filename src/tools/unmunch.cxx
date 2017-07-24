83451f1234248cbc54191e80b8bd4597a531674a
Mikhail Paulyshka <me@mixaill.tk>
2017-07-25 01:55:13 +0300

    tools/unmunch: fix build with MSVC

--- src/tools/unmunch.cxx
+++ src/tools/unmunch.cxx
@@ -42,7 +42,9 @@
 #include <ctype.h>
 #include <string.h>
 #include <string>
+#ifdef HAVE_UNISTD_H
 #include <unistd.h>
+#endif /* HAVE_UNISTD_H */
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdio.h>
