563aeabece1a623764a51b286bac0b4d571e095d
Pander <pander@users.sourceforge.net>
2017-09-08 19:57:18 +0200

    reformatted tests with clang

--- tests/hun2/string_utils_test.cxx
+++ tests/hun2/string_utils_test.cxx
@@ -25,23 +25,22 @@ TEST (SplitTest, PositiveNos) {
 }
 */
 
-int main(int argc, char* argv[])
-{
-	vector<string> ztrings;
+int main(int argc, char *argv[]) {
+  vector<string> ztrings;
 
-	ztrings.insert(ztrings.end(), string("end"));
-	ztrings.insert(ztrings.begin(), string("begin"));
+  ztrings.insert(ztrings.end(), string("end"));
+  ztrings.insert(ztrings.begin(), string("begin"));
 
-	for (vector<string>::iterator ztring = ztrings.begin();
-	     ztring < ztrings.end(); ++ztring) {
-		cout << ztring->c_str() << endl;
-	}
+  for (vector<string>::iterator ztring = ztrings.begin();
+       ztring < ztrings.end(); ++ztring) {
+    cout << ztring->c_str() << endl;
+  }
 
-	//    ztrings = split(string("asdf;qwer"), ';', ztrings);
+  //    ztrings = split(string("asdf;qwer"), ';', ztrings);
 
-	for (auto& ztring : ztrings) {
-		cout << ztring.c_str() << endl;
-	}
+  for (auto &ztring : ztrings) {
+    cout << ztring.c_str() << endl;
+  }
 
-	return 0;
+  return 0;
 }
