a8b03d18c9f4e765aa48774f6c6544d494830069
Dimitrij Mijoski <dmjpp@hotmail.com>
2017-11-08 18:33:05 +0100

    v2 cmd line tool typo in licence

--- src/hunspell2/main.cxx
+++ src/hunspell2/main.cxx
@@ -205,7 +205,7 @@ auto print_version() -> void
 {
 	cout << "hunspell 2.0.0\n"
 		"Copyright (C) 2017 Dimitri Mijoski and Sander van Geloven\n"
-		"License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>."
+	        "License LGPLv3+: GNU LGPL version 3 or later <http://gnu.org/licenses/lgpl.html>."
 		"This is free software: you are free to change and redistribute it."
 		"There is NO WARRANTY, to the extent permitted by law.\n"
 		"\n"
