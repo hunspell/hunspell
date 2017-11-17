58dfe79637982c5c49658c57c3b01d4f44c07c19
Dimitrij Mijoski <dmjpp@hotmail.com>
2017-11-17 19:22:42 +0100

    Revert "License everything under LGPLv3+. No more three licenses mumbo jumbo."
    This reverts commit d49170ce949dbe0d2e6ad74b6b876e5580704a5e.

--- README.md
+++ README.md
@@ -38,7 +38,8 @@ Main features of Hunspell spell checker and morphological analyzer:
     Azeri and Turkish dotted i, or German sharp s)
   - Handle conditional affixes, circumfixes, fogemorphemes, forbidden
     words, pseudoroots and homonyms.
-  - Free software. Licenced under LGPLv3+.
+  - Free software. Versions 1.x are licenced under LGPL, GPL, MPL
+    tri-license. Version 2 is licenced only under GNU LGPL.
 
 # Dependencies
 
--- src/tools/hunspell.cxx
+++ src/tools/hunspell.cxx
@@ -1,20 +1,40 @@
-/* Copyright 2002-2017 Németh László
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1/GPL 2.0/LGPL 2.1
  *
- * This file is part of Hunspell.
+ * Copyright (C) 2002-2017 Németh László
  *
- * Hunspell is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
  *
- * Hunspell is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
  *
- * You should have received a copy of the GNU Lesser General Public License
- * along with Hunspell.  If not, see <http://www.gnu.org/licenses/>.
- */
+ * Hunspell is based on MySpell which is Copyright (C) 2002 Kevin Hendricks.
+ *
+ * Contributor(s): David Einstein, Davide Prina, Giuseppe Modugno,
+ * Gianluca Turconi, Simon Brouwer, Noll János, Bíró Árpád,
+ * Goldman Eleonóra, Sarlós Tamás, Bencsáth Boldizsár, Halácsy Péter,
+ * Dvornik László, Gefferth András, Nagy Viktor, Varga Dániel, Chris Halls,
+ * Rene Engelhard, Bram Moolenaar, Dafydd Jones, Harri Pitkänen
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ *
+ * ***** END LICENSE BLOCK ***** */
 
 // glibc < 3.0 (for mkstemp)
 #ifndef __USE_MISC
@@ -1855,10 +1875,11 @@ int main(int argc, char** argv) {
       fprintf(stdout, "\n");
       if (strcmp(argv[i], "-vv") != 0) {
         fprintf(stdout, "%s",
-	        gettext("\nCopyright (C) 2002-2017 L\303\241szl\303\263 "
-	                "N\303\251meth. License: LGPLv3+.\n\n"
+                gettext("\nCopyright (C) 2002-2014 L\303\241szl\303\263 "
+                        "N\303\251meth. License: MPL/GPL/LGPL.\n\n"
                         "Based on OpenOffice.org's Myspell library.\n"
-	                "Myspell's copyright (C) Kevin Hendricks, 2001-2002.\n\n"));
+                        "Myspell's copyright (C) Kevin Hendricks, 2001-2002, "
+                        "License: BSD.\n\n"));
         fprintf(stdout, "%s", gettext("This is free software; see the source for "
                                 "copying conditions.  There is NO\n"
                                 "warranty; not even for MERCHANTABILITY or "
