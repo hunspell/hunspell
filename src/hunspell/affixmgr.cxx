hunspell/hunspell

commit 1fada01663b29b57c010a9c274e45a5cf9ecf222

fix other regression in compounding
Allow compound words again with
starting "kor", "alak", "asszony", "úr"
related to the "REP kor _kor" etc. rules
using the Hungarian spelling dictionary.

regression from...

commit 73b1cad1af7ab94252f75784fa6724cf062a6966
Author: Martin Hosken <martin_hosken@sil.org>
Date:   Mon Apr 18 16:28:26 2016 +0700

    Add support for bounded conversion

src/hunspell/affixmgr.cxx
@@ -1290,8 +1290,8 @@ int AffixMgr::cpdrep_check(const char* word, int wl) {
     // search every occurence of the pattern in the word
     while ((r = strstr(r, reptable[i].pattern.c_str())) != NULL) {
       std::string candidate(word);
-      size_t type = r == word ? 1 : 0;
-      if (r - word + reptable[i].pattern.size() == lenp)
+      size_t type = r == word && langnum != LANG_hu ? 1 : 0;
+      if (r - word + reptable[i].pattern.size() == lenp && langnum != LANG_hu)
         type += 2;
       candidate.replace(r - word, lenp, reptable[i].outstrings[type]);
       if (candidate_check(candidate.c_str(), candidate.size()))

commit 0b8a4d8851c94485dcc13cf8b8688c8d3fb9a783

Use only middle replentries for compound word checking
allowing compound filtering for compound stems and affixed
forms in every languages.

This replaces the partial fix for the CHECKCOMPOUNDREP regression
in commit 1fada01663b29b57c010a9c274e45a5cf9ecf222.

src/hunspell/affixmgr.cxx
@@ -1273,18 +1273,18 @@ int AffixMgr::cpdrep_check(const char* word, int wl) {
     return 0;
 
   for (size_t i = 0; i < get_reptable().size(); ++i) {
-    const char* r = word;
-    const size_t lenp = get_reptable()[i].pattern.size();
-    // search every occurence of the pattern in the word
-    while ((r = strstr(r, get_reptable()[i].pattern.c_str())) != NULL) {
-      std::string candidate(word);
-      size_t type = r == word && langnum != LANG_hu ? 1 : 0;
-      if (r - word + get_reptable()[i].pattern.size() == lenp && langnum != LANG_hu)
-        type += 2;
-      candidate.replace(r - word, lenp, get_reptable()[i].outstrings[type]);
-      if (candidate_check(candidate.c_str(), candidate.size()))
-        return 1;
-      ++r;  // search for the next letter
+    // use only available mid patterns
+    if (!get_reptable()[i].outstrings[0].empty()) {
+      const char* r = word;
+      const size_t lenp = get_reptable()[i].pattern.size();
+      // search every occurence of the pattern in the word
+      while ((r = strstr(r, get_reptable()[i].pattern.c_str())) != NULL) {
+        std::string candidate(word);
+        candidate.replace(r - word, lenp, get_reptable()[i].outstrings[0]);
+        if (candidate_check(candidate.c_str(), candidate.size()))
+          return 1;
+        ++r;  // search for the next letter
+      }
     }
   }
 