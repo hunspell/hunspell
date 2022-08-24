799b71bda14a48fe1264cd68b8ef5c1c517be4f2
Caolán McNamara <caolanm@redhat.com>
2022-08-24 10:55:52 +0100

    move oss-fuzz build into project (#789)

---
+++ oss-fuzz-build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ -z "${OUT}" ] || [ -z "${SRC}" ] || [ -z "${WORK}" ]; then
+    echo "OUT, SRC or WORK not set - script expects to be called inside oss-fuzz build env"
+    exit 1
+fi
+
+autoreconf -vfi
+./configure --disable-shared --enable-static
+make clean
+make -j$(nproc)
+$CXX $CXXFLAGS -o $OUT/fuzzer -I./src/ $LIB_FUZZING_ENGINE ./src/tools/fuzzer.cxx ./src/hunspell/.libs/libhunspell-1.7.a
+
+#dic/aff combos to test
+cp -f ./tests/korean.* $OUT/
+cp -f ./tests/utf8_nonbmp.* $OUT/
