7cc8bee0d08666f95f2dd49985cd76e9f0af5d8f
Caolán McNamara <caolanm@redhat.com>
2022-09-05 19:57:03 +0100

    check both args of AffixMgr::isSubset

--- src/hunspell/affixmgr.cxx
+++ src/hunspell/affixmgr.cxx
@@ -1067,7 +1067,7 @@ int AffixMgr::encodeit(AffEntry& entry, const std::string& cs) {
 
 // return 1 if s1 is a leading subset of s2 (dots are for infixes)
 inline int AffixMgr::isSubset(const char* s1, const char* s2) {
-  while (((*s1 == *s2) || (*s1 == '.')) && (*s1 != '\0')) {
+  while (((*s1 == *s2) || (*s1 == '.')) && (*s1 != '\0') && (*s2 != '\0')) {
     s1++;
     s2++;
   }
