ea079a83d501f123bef3221150361a7e9d8e24f3
Caolán McNamara <caolanm@redhat.com>
2022-09-05 16:19:53 +0100

    do suffix_check_twosfx_morph

--- src/hunspell/affixmgr.hxx
+++ src/hunspell/affixmgr.hxx
@@ -232,7 +232,7 @@ class AffixMgr {
                                         int len,
                                         char in_compound,
                                         const FLAG needflag = FLAG_NULL);
-  std::string suffix_check_twosfx_morph(const char* word,
+  std::string suffix_check_twosfx_morph(const std::string& word,
                                         int start,
                                         int len,
                                         int sfxopts,
