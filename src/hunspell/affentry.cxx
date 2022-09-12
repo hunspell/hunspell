89efe61594c02706612d8555ce7d0832d8a4704d
Caolán McNamara <caolanm@redhat.com>
2022-09-12 09:26:16 +0100

    ofz#51242 bad offset with dubious aff

--- src/hunspell/affentry.cxx
+++ src/hunspell/affentry.cxx
@@ -469,7 +469,7 @@ std::string SfxEntry::add(const char* word, size_t len) {
   if ((len > strip.size() || (len == 0 && pmyMgr->get_fullstrip())) &&
       (len >= numconds) && test_condition(word + len, word) &&
       (!strip.size() ||
-       (strcmp(word + len - strip.size(), strip.c_str()) == 0))) {
+       (len >= strip.size() && strcmp(word + len - strip.size(), strip.c_str()) == 0))) {
     result.assign(word, len);
     /* we have a match so add suffix */
     result.replace(len - strip.size(), std::string::npos, appnd);
