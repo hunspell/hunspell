06df946204dffff07ee808d82e26c365b477fba8
Caolán McNamara <caolanm@redhat.com>
2022-10-07 09:11:34 +0100

    ofz#52195 check bounds
    it might be we wanted st here not word

--- src/hunspell/affixmgr.cxx
+++ src/hunspell/affixmgr.cxx
@@ -1879,7 +1879,7 @@ struct hentry* AffixMgr::compound_check(const std::string& word,
               if (striple) {
                 checkedstriple = 1;
                 i--;  // check "fahrt" instead of "ahrt" in "Schiffahrt"
-              } else if (i > 2 && word[i - 1] == word[i - 2])
+              } else if (i > 2 && i <= word.size() && word[i - 1] == word[i - 2])
                 striple = 1;
             }
 
