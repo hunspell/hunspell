hunspell/hunspell

commit ce8071b9d3312b3599714fc36fcc3fac11cdae97

ofz#51514 std::out_of_range use substring a std::string::find

src/hunspell/affixmgr.cxx
@@ -1282,20 +1282,22 @@ std::string AffixMgr::prefix_check_twosfx_morph(const std::string& word,
 }
 
 // Is word a non-compound with a REP substitution (see checkcompoundrep)?
-int AffixMgr::cpdrep_check(const std::string& word, int wl) {
+int AffixMgr::cpdrep_check(const std::string& in_word, int wl) {
 
   if ((wl < 2) || get_reptable().empty())
     return 0;
 
+  std::string word(in_word, 0, wl);
+
   for (size_t i = 0; i < get_reptable().size(); ++i) {
     // use only available mid patterns
     if (!get_reptable()[i].outstrings[0].empty()) {
-      const char* r = word.c_str();
+      size_t r = 0;
       const size_t lenp = get_reptable()[i].pattern.size();
       // search every occurence of the pattern in the word
-      while ((r = strstr(r, get_reptable()[i].pattern.c_str())) != NULL) {
-        std::string candidate(word.c_str());
-        candidate.replace(r - word.c_str(), lenp, get_reptable()[i].outstrings[0]);
+      while (word.find(get_reptable()[i].pattern, r) != std::string::npos) {
+        std::string candidate(word);
+        candidate.replace(r, lenp, get_reptable()[i].outstrings[0]);
         if (candidate_check(candidate))
           return 1;
         ++r;  // search for the next letter

commit eaae126613d576d2afa21ab3b13c18bc1cb81a1d

Fix regression in CHECKCOMPOUNDREP
Return value of string::find() was ignored in AffixMgr::cpdrep_check(),
applying REP replacements at all positions before the last matching
position.

For example a regression in Hungarian spell checking:

Correct closed compound word "fa+ajtó" was rejected: the ó -> o replacement
applied on the second character position, replacing "aa" with "o" there
(2-character length of UTF-8 character "ó"), and the result "fojtó"
is a correct dictionary word, which was the reason for rejection.

Regression from commit ce8071b9d3312b3599714fc36fcc3fac11cdae97
"ofz#51514 std::out_of_range use substring a std::string::find".

src/hunspell/affixmgr.cxx
@@ -1286,7 +1286,7 @@ int AffixMgr::cpdrep_check(const std::string& in_word, int wl) {
       size_t r = 0;
       const size_t lenp = i.pattern.size();
       // search every occurence of the pattern in the word
-      while (word.find(i.pattern, r) != std::string::npos) {
+      while ((r = word.find(i.pattern, r)) != std::string::npos) {
         std::string candidate(word);
         candidate.replace(r, lenp, i.outstrings[0]);
         if (candidate_check(candidate))

tests/checkcompoundrep2.aff
@@ -1,0 +1,18 @@
+# check fix of regression of commit ce8071b9d3312b3599714fc36fcc3fac11cdae97:
+# Return value of string.find() was ignored in AffixMgr::cpdrep_check(),
+# applying REP replacements at all positions before the last matching position.
+#
+# For example a regression in Hungarian spell checking:
+#
+# Correct closed compound word "fa+ajtó" was rejected: the ó -> o replacement
+# applied on the second character position, replacing "aa" with "o" there
+# (2-character length of UTF-8 character "ó"), and the result "fojtó"
+# is a correct dictionary word, which was the reason for rejection.
+SET UTF-8
+
+COMPOUNDMIN 2
+COMPOUNDFLAG x
+CHECKCOMPOUNDREP
+
+REP 1
+REP ó o

tests/checkcompoundrep2.dic
@@ -1,0 +1,4 @@
+2
+fa/x
+ajtó/x
+fojtó

tests/checkcompoundrep2.good
@@ -1,0 +1,4 @@
+fa
+ajtó
+ajtófa
+faajtó